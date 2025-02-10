976804c1870872e5a51f1e90ac251b86b3ca6a48
VidiraVibhute <164131643+VidiraVibhute@users.noreply.github.com>
2025-02-10 09:21:32 +0530

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,71 +1,78 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from blog import models
-from .models import Post
 from django.contrib.auth import authenticate, login, logout
+from django.views import View
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+from .models import Post
 
+class SignupView(View):
+    template_name = 'blog/signup.html'
 
+    def get(self, request):
+        return render(request, self.template_name)
 
-
-def signup(request):
-    if request.method == 'POST':
+    def post(self, request):
         name = request.POST.get('uname')
-        email= request.POST.get('uemail')
+        email = request.POST.get('uemail')
         password = request.POST.get('upassword')
-        newUser = User.objects.create_user(username=name, email=email, password=password)
-        newUser.save()
+
+        new_user = User.objects.create_user(username=name, email=email, password=password)
+        new_user.save()
+        
         return redirect('/loginn')
-    return render(request, 'blog/signup.html')
 
 
+class LoginView(View):
+    template_name = 'blog/login.html'
 
+    def get(self, request):
+        return render(request, self.template_name)
 
-def loginn(request):
-    if request.method == 'POST':
+    def post(self, request):
         name = request.POST.get('uname')
         password = request.POST.get('upassword')
-        userr = authenticate(request, username=name, password=password)
-        if userr is not None:
-            login(request, userr)
+
+        user = authenticate(request, username=name, password=password)
+        if user is not None:
+            login(request, user)
             return redirect('/home')
         else:
-            return redirect('/login')
-
-    return render(request, 'blog/login.html')
+            return redirect('/loginn')
 
 
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('/loginn')
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+class HomeView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
 
 
+class NewPostView(View):
+    template_name = 'blog/newpost.html'
 
-def newPost(request):
-    if request.method == 'POST':
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
         title = request.POST.get('title')
         content = request.POST.get('content')
-        npost = models.Post(title=title, content=content, author=request.user)
-        npost.save()
+
+        new_post = Post(title=title, content=content, author=request.user)
+        new_post.save()
+
         return redirect('/home')
-    
-    return render(request, 'blog/newpost.html')
 
 
+class MyPostView(ListView):
+    model = Post
+    template_name = 'blog/mypost.html'
+    context_object_name = 'posts'
 
-
-def myPost(request):
-    context = {
-        'posts': Post.objects.filter(author= request.user)
-    }
-    return render(request, 'blog/mypost.html', context)
-
-
-
-def signout(request):
-    logout(request)
-    return redirect('/loginn')
-
+    def get_queryset(self):
+        return Post.objects.filter(author=self.request.user)
