7a4efd3ff12a40907d83a6bc6dde288c8cc85dfc
VidiraVibhute <164131643+VidiraVibhute@users.noreply.github.com>
2025-02-10 09:22:11 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from . import views
+from .views import SignupView, LoginView, LogoutView, HomeView, NewPostView, MyPostView
 
 urlpatterns = [
-    path('', views.signup, name='signup-page'),
-    path('loginn/', views.loginn, name='login-page'),
-    path('home/', views.home, name='home-page'),
-    path('newpost/', views.newPost, name='new-post'),
-    path('mypost/', views.myPost, name='my-post'),
-    path('signout/', views.signout, name='signout-btn'),
-    
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('loginn/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('home/', HomeView.as_view(), name='home'),
+    path('newpost/', NewPostView.as_view(), name='newpost'),
+    path('myposts/', MyPostView.as_view(), name='myposts'),
 ]
